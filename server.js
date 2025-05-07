924c4011a473a7781a02714c8cb272cb54498910
RAMON MULLER <ramon_muller1@estudante.sesisenai.org.br>
2025-05-07 11:14:16 -0300

    Bug fixes e finalização

--- server.js
+++ server.js
@@ -13,26 +13,24 @@ app.get('/logs', (req, res) => {
     myReadFile('./logs.txt', res)
 })
 
+app.get('/logs/:id', (req, res) => {
+    const id = String(req.params.id);
+    if (!id) return res.status(404).send('You must pass a valid ID');
+    
+    getLog('./logs.txt', id)
+        .then(log => res.status(200).send(log))
+        .catch(err => res.status(500).send(err));
+})
+
 app.post('/logs', (req, res) => {
     if (!req.body) return res.status(400).send('You must pass a name in the body');
 
-    newLog('logs.txt' , req.body).then(log => {
+    newLog('./logs.txt' , req.body).then(log => {
         res.status(201).send(log);
     }).catch(err => {
         res.status(500).send(err);
     });
 })
-
-
-app.get('/logs/:id', (req, res) => {
-    const id = String(req.params.id);
-    if (!id) return res.status(404).send('You must pass a valid ID');
-    
-    getLog('logs.txt', id)
-        .then(log => res.status(200).send(log))
-        .catch(err => res.status(500).send(err));
-})
-
 
 
 
--- store-functions.js
+++ store-functions.js
@@ -1,18 +1,37 @@
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
 
-const myReadFile = (arquivo, res) => {
-    fs.readFile(arquivo, 'utf-8', (err, data) => { //abrndo o arquivo database e lendo ele, em formato utf-8
-        console.log(data)
-        res.send(data)
+
+
+
+function writeFile(arquivo, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(arquivo, data, 'utf-8', (err) => {
+            if (err) return reject(err);
+            resolve(null);
+        })
+    })
+
+}
+
+const myReadFile = (arquivo) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(arquivo, 'utf-8', (err, data) => {
+            if (err) return reject(err); 
+            console.log(data);
+            resolve(data);
+        })
     })
 }
 
-async function newLog(arquivo , name) {
+async function newLog(arquivo, name) {
+    const uuid = uuidv4();
     const date = new Date();
+    console.log(date);
     const log = `${uuid} - ${date} - ${name}`;
+    console.log(log);
 
-    const allLogs = await readFile(arquivo);
+    const allLogs = await myReadFile(arquivo);
     await writeFile(arquivo, allLogs + "\n" + log);
     return log;
 }
@@ -24,26 +43,18 @@ function getLog(arquivo, id) {
             let log = undefined;
 
             String(data).split('\n').forEach(line => {
-                if (line.substring(0, id.length) !== id) return;
-                log = line;
-            })
-            if (log) resolve(log);
-            reject(new Error("No log found with specified ID"));
+                if (line.startsWith(id)) {
+                    log = line;
+                }
+            });
+
+            if (log) {
+                resolve(log);
+            } else {
+                reject(new Error("No log found with specified ID"));
+            }
         }).catch(err => reject(err));
-    })
+    });
 }
 
-
-module.exports = { myReadFile, getLog , newLog }
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = { myReadFile, getLog, newLog }
